e432955acd5b7b6b227c439277974de932bbdd13
liuyang_admin <liuyang_admin@163.com>
2021-08-18 15:12:30 +0800

    Initial commit

---
+++ src/main/java/com/hot/sauce/HotSauceApplication.java
@@ -0,0 +1,13 @@
+package com.hot.sauce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotSauceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotSauceApplication.class, args);
+    }
+
+}
